5775ff2c623a28dcd96640f0c08a19e38e24b6ab
ori-code <88030092+OriDoboshZippit@users.noreply.github.com>
2022-05-24 10:33:42 +0300

    works with nodemon

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 
 const mongoose  = require ('./database/mongoose');
 app.listen(3000, function(){
-    console.log("Server started on port 3000");
+    console.log("Server started on port 300000!");
 });
